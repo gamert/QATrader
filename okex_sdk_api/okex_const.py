bdcc1765388256ace04aee33c62de90deabbd3ef
xiaozhi <20785672@qq.com>
2021-03-03 18:09:11 +0800

    add: okex api

---
+++ okex_sdk_api/okex_const.py
@@ -0,0 +1,112 @@
+# coding=utf-8
+"""
+    OKEX cosnt...
+
+"""
+
+ok_ws_url = 'wss://real.okex.com:8443/ws/v3'
+
+# 现货
+# 用户币币账户频道
+# channels = ["spot/account:USDT"]
+# channels = ["spot/account:XRP", "futures/account:XRP", "swap/account:XRP-USD-SWAP"]
+# 用户杠杆账户频道
+# channels = ["spot/margin_account:BTC-USDT"]
+# 用户委托策略频道
+# channels = ["spot/order_algo:XRP-USDT"]
+# 用户交易频道
+# channels = ["spot/order:XRP-USDT"]
+# 公共-Ticker频道
+# channels = ["spot/ticker:EOS-USDT"]
+# 公共-K线频道
+# channels = ["spot/candle60s:ETH-USDT"]
+# 公共-交易频道
+# channels = ["spot/trade:BTC-USDT"]
+# 公共-5档深度频道
+# channels = ["spot/depth5:PLG-USDT"]
+# 公共-400档深度频道
+# channels = ["spot/depth:BTC-USDT"]
+# 公共-全量深度频道
+# channels = ["spot/depth_l2_tbt:XRP-USDT"]
+
+# 交割合约
+# 用户持仓频道
+# channels = ["futures/position:XRP-USD-200327"]
+# 用户账户频道
+# channels = ["futures/account:BTC-USDT"]
+# 用户交易频道
+# channels = ["futures/order:ETH-USD-200117"]
+# 用户委托策略频道
+# channels = ["futures/order_algo:XRP-USD-200327"]
+# 公共-全量合约信息频道
+# channels = ["futures/instruments"]
+# 公共-Ticker频道
+# channels = ["futures/ticker:BTC-USD-200327"]
+# 公共-K线频道
+# channels = ["futures/candle60s:BTC-USD-200327"]
+# 公共-交易频道
+# channels = ["futures/trade:BTC-USD-200117"]
+# 公共-预估交割价频道
+# channels = ["futures/estimated_price:BTC-USD-200327"]
+# 公共-限价频道
+# channels = ["futures/price_range:BTC-USD-200327"]
+# 公共-5档深度频道
+# channels = ["futures/depth5:BTC-USD-200327"]
+# 公共-400档深度频道
+# channels = ["futures/depth:BSV-USD-200327"]
+# 公共-全量深度频道
+# channels = ["futures/depth_l2_tbt:BTC-USD-200327"]
+# 公共-标记价格频道
+# channels = ["futures/mark_price:BTC-USD-200327"]
+
+# 永续合约
+# 用户持仓频道
+# channels = ["swap/position:BTC-USD-SWAP"]
+# 用户账户频道
+# channels = ["swap/account:BTC-USD-SWAP"]
+# 用户交易频道
+# channels = ["swap/order:BTC-USD-SWAP"]
+# 用户委托策略频道
+# channels = ["swap/order_algo:LTC-USD-SWAP"]
+# 公共-Ticker频道
+# channels = ["swap/ticker:BTC-USD-SWAP"]
+# 公共-K线频道
+# channels = ["swap/candle60s:BTC-USD-SWAP"]
+# 公共-交易频道
+# channels = ["swap/trade:BTC-USD-SWAP"]
+# 公共-资金费率频道
+# channels = ["swap/funding_rate:BTC-USD-SWAP"]
+# 公共-限价频道
+# channels = ["swap/price_range:BTC-USD-SWAP"]
+# 公共-5档深度频道
+# channels = ["swap/depth5:BTC-USD-SWAP"]
+# 公共-200档深度频道
+# channels = ["swap/depth:BTC-USDT-SWAP"]
+# 公共-全量深度频道
+# channels = ["swap/depth_l2_tbt:XRP-USD-SWAP"]
+# 公共-标记价格频道
+# channels = ["swap/mark_price:BTC-USD-SWAP"]
+
+# 期权合约
+# 用户持仓频道
+# channels = ["option/position:BTC-USD"]
+# 用户账户频道
+# channels = ["option/account:BTC-USD"]
+# 用户交易频道
+# channels = ["option/order:BTC-USD"]
+# 公共-合约信息频道
+# channels = ["option/instruments:BTC-USD"]
+# 公共-期权详细定价频道
+# channels = ["option/summary:BTC-USD"]
+# 公共-K线频道
+# channels = ["option/candle60s:BTC-USD-200327-11000-C"]
+# 公共-最新成交频道
+# channels = ["option/trade:BTC-USD-200327-11000-C"]
+# 公共-Ticker频道
+# channels = ["option/ticker:BTC-USD-200327-11000-C"]
+# 公共-5档深度频道
+# channels = ["option/depth5:BTC-USD-200327-11000-C"]
+# 公共-400档深度频道
+# channels = ["option/depth:BTC-USD-200327-11000-C"]
+# 公共-全量深度频道
+# channels = ["option/depth_l2_tbt:BTC-USD-200327-11000-C"]
